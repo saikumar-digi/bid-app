6001c82941f28e1ffc3d9925c86a69d8c64c44e9
saikumar <saikumar@digisprint.com>
2023-04-28 10:15:57 +0530

    Removed folder from repository

--- client/src/services/players.service.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../utilities/axiosInstance";
-
-const fetchSquads = async () => {
-  return await axiosInstance.get("players").then((response) => {
-    return response.data;
-  });
-};
-
-export { fetchSquads };

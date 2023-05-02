e486178678787c5c5d3a15c4894fdaca5031bb8c
saikumar <saikumar@digisprint.com>
2023-05-02 17:10:17 +0530

    delete clint

--- client/src/services/auction.service.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../utilities/axiosInstance";
-
-const fetchAuctions = async () => {
-  return await axiosInstance.get("played-auctions").then((response) => {
-    return response.data;
-  });
-};
-
-export { fetchAuctions };
